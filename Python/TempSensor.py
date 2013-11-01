faafc84322a38b84be6e58f34885c68217c697df
Sarah Woodrow <sarah.woodrow@physics.ox.ac.uk>
2013-11-01 15:30:54 +0000

    Timer working ok, adding code for user input (using threading)

--- Python/TempSensor.py
+++ Python/TempSensor.py
@@ -16,11 +16,11 @@ print s
 # When serial port is opened, Arduino resets. Delay to ensure commands are received properly.
 time.sleep(3)	
 
-#instruction = None						# Create empty string
+instruction = None						# Create empty string
 
 def measure():
-	ser.write("T")						# Send test signal to make sure the serial is working
-	print ser.readline()				# Print response for debugging
+#	ser.write("T")						# Send test signal to make sure the serial is working
+#	print ser.readline()				# Print response for debugging
 
 	ser.write("M")						# Send signal to take a measurement
 	RH = ser.readline()					# First line output from Arduino is the relative humidity
@@ -28,20 +28,38 @@ def measure():
 	temp = ser.readline()				# Second line is the temperature
 	print temp
 
-# Timer which will execute function 'measure' every 'period' seconds
-#myTimer = threading.Timer(period, measure)
-#myTimer.start()							# Start timer
-
-
 def repeat(interval, myFunction ):
-	threading.Timer( interval, repeat, [interval, myFunction] ).start()
+	global variable
+	if variable < 5:
+		variable = variable + 1
+		global myTimer
+		myTimer = threading.Timer( interval, repeat, [interval, myFunction] )
+		myTimer.start()
+		myFunction()
+		print variable
+		
+# Timer which will execute function 'measure' every 'period' seconds
+# myTimer = threading.Timer(period, measure)
+# myTimer.start()							# Start timer
 
-	myFunction()
 
+variable = 0
+
+# myTimer = threading.Timer(period, repeat, [period, measure, myVar] )
 repeat(period, measure )
 
+# s = "Testing - variable = " + `myVar`
+# print s
 
+def get_input():
+	global instruction = raw_input("Type instructions:")
 
-# Code goes here to accept input from the user
-# and change the period of measurement (stop Timer, change period, restart timer)
+myTimer.join()
+print "Timer finished"
+print variable
+
+# while 1:
+	# inputThread = threading.Thread(target=get_input)
+	# inputThread.join()
+
 
