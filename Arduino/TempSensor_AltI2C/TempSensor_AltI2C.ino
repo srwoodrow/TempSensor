70c200403c79b134ed372afc4dbf1edbd34d3472
Sarah Woodrow <sarah.woodrow@physics.ox.ac.uk>
2013-11-05 14:42:19 +0000

    Test code written to check if each chip is working (to use with scope)

---
+++ Arduino/TempSensor_AltI2C/TempSensor_AltI2C.ino
@@ -0,0 +1,180 @@
+// Firmware for Arduino Uno, to interface with Honeywell HIH6130 humidity/temperature sensor and ADT7310 temperature sensor
+
+
+// Communication libraries
+#include <Wire.h>       // I2C library for HIH6130 humidity sensor
+#include <SPI.h>        // SPI library for ADT7310 temperature sensor
+
+// Pins used on Arduino Uno
+#define SDAPin A4
+
+#define SCK 13          // (SPI) Serial clock
+#define MISO 12         // (SPI) Master In Slave Out - send data to Master (Arduino)
+#define MOSI 11         // (SPI) Master Out Slave In - send data to Slave (temp sensor)
+#define SS 10           // (SPI) Slave select
+
+// Sensor SPI command bytes
+#define READ_TEMP_REG            0x50  // Read from temperature register
+#define READ_ID_REG              0x58  // Read from ID register
+#define WRITE_TO_CONFIG_REG      0x08  // Write to configuration register
+#define SIXTEEN_BIT_MODE         0x80  // Enable 16bit resolution (13bit is default)
+#define ENABLE_ONE_SHOT_MODE     0x20  // Enable single-shot conversion mode (default is continuous conversion)
+
+
+float temperature;                     // Current temperature of the ADT7310
+unsigned int conversionDelay = 250;    // Time taken to perform a temperature conversion (mS) specified min is 240mS
+
+
+int HIH_Address = 0x27;                // Address of HIH6130
+uint16_t humidity, HIH_temp = 0;       // Current humidity & temp reading from HIH6130
+byte HIH_Status = 0;                   // Status byte from the HIH6130
+float rel_humidity = 0.0;              // Float to store relative humidity
+
+
+void setup()
+{
+  Serial.begin(9600);              // Establish serial comms with PC
+  
+  Wire.begin();                    // Join I2C bus as master
+  pinMode(SDAPin, OUTPUT);            // Set I2C data line as an output
+  digitalWrite(SDAPin, HIGH);         // Turn on HIH6130
+  
+  SPI.begin();                            // This sets SCK, MOSI, and SS to outputs and pulls MOSI low and SCK & CS high. 
+  SPI.setDataMode(SPI_MODE3);             // (CPOL=1, CPHA=1)
+  SPI.setBitOrder(MSBFIRST);              // Set bit order to MSB first
+  SPI.setClockDivider(SPI_CLOCK_DIV8);    // The sensor needs a min clock period of 200ns (5MHz). Arduino Uno runs at 16MHz, so this gives 2MHz
+  pinMode(MISO, INPUT);  
+  
+  // Make sure that the ADT7310 chip is disabled
+  pinMode(SS, OUTPUT);                   // Set SS pin as output
+  digitalWrite(SS, HIGH);                // Set SS pin high (device disabled)
+  resetSPI();                            // Put bus into a known state (digital pins strobing on startup can confuse the sensors)
+  
+}
+
+void loop()
+{
+  while(Serial.available() > 0)    // Wait until bytes are available on the serial port
+  {
+    char incomingByte = Serial.read();                     // Read the byte
+    if(incomingByte == 'T'|| incomingByte == 't')          // Test case
+    {
+      Serial.println("Test command received");  
+    }
+    else if(incomingByte == 'M' || incomingByte == 'm')    // Signal to take a measurement
+    {
+      measure();
+      delay(100);
+    }
+  }
+  
+  
+}
+
+
+void measure()
+{
+  // Get temperature data from the ADT7310
+  fetch_temp();
+  Serial.print("T = ");
+  Serial.println(temperature);
+  
+  // Get humidity data from HIH6130 - error byte should be zero if no errors
+  byte hum_error = fetch_humidity();
+  Serial.println("humidity fetched");
+  if(hum_error == 0)      // If there were no errors
+  {
+    rel_humidity = (float)humidity / 16383 * 100;    // Convert raw data to relative humidity
+    Serial.print("RH = ");
+    Serial.println(rel_humidity);    
+  }  
+  else
+  {
+    Serial.print("RH Error ");
+    Serial.println(hum_error);
+  }  
+}
+
+
+
+byte fetch_humidity()
+{
+  byte data[4];             // To store received data from the chip (4 bytes)          
+  boolean error = false;    // Error flag to check we have received the right number of bytes (4)  
+  
+  Wire.beginTransmission(HIH_Address);
+  Serial.println("Beginning transmission");
+  Wire.endTransmission();
+  Serial.println("Ended transmission");
+  delay(100);
+  Serial.println("Chip addressed");
+ 
+  Wire.requestFrom(HIH_Address, 4);    // Request 4 bytes from the sensor
+  
+  // We expect 4 bytes from the chip
+  for(int i = 0; i < 4; i++)
+  {
+    // Make sure there are bytes available (it may transmit fewer bytes than expected)
+    if(Wire.available())
+    {
+       data[i] = Wire.read();
+    } 
+    else
+    {
+      error = true;    // Flag an error
+    }
+  }
+  Wire.endTransmission();
+
+  // Only process if there is no error
+  if(!error)
+  {
+    HIH_Status = (data[0] >> 6) & B00000011;    // Extract status bits
+    humidity = word( (data[0] & B00111111), data[1]);
+    HIH_temp = word(data[2], data[3]);
+    HIH_temp = HIH_temp >> 2;        // Drop the last 2 bits (Do Not Care)
+    
+    return HIH_Status;
+  }
+  else 
+    return 4;
+}
+
+
+void fetch_temp()
+{
+  // Request conversion for temp sensor 
+  digitalWrite(SS, LOW);                // Enable device
+  SPI.transfer(WRITE_TO_CONFIG_REG);
+  SPI.transfer(ENABLE_ONE_SHOT_MODE | SIXTEEN_BIT_MODE);
+  digitalWrite(SS, HIGH);               // Disable device
+  
+  delay(conversionDelay);               // Delay to allow sensor to perform conversion
+  
+  // Read temperatures of sensor
+  digitalWrite(SS, LOW);                // Enable device
+  SPI.transfer(READ_TEMP_REG);
+  byte tempData [2];
+  tempData [1] = SPI.transfer(0x00);
+  tempData [0] = SPI.transfer(0x00);
+  digitalWrite(SS, HIGH);               // Disable device
+
+  uint16_t  tempInt = *((uint16_t*)(tempData));
+  if( 0x80 & tempData[1] )                           // MSB is sign bit
+   temperature = ((float)(tempInt - 65536))/128;     // Negative temperature
+  else
+    temperature = ((float)tempInt)/128;              // Positive temperature
+}
+
+
+void resetSPI()      // Reset the ADT7310 on the bus by writing a series of 32 1s
+{
+  digitalWrite(SS, LOW);	// Enable device
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);
+  SPI.transfer(0xFF);  
+  digitalWrite(SS, HIGH);       // Disable device
+  delay(1);  // Make sure sensor has plenty of time to reset  
+}
+
---
+++ Python/TempSensorSingle.py
@@ -0,0 +1,129 @@
+#
+import serial
+import time
+import threading
+
+##############################################
+# Global variables
+##############################################
+interval = 1.0								# Measurement interval (in seconds)
+folder = "H:\\TempSensorTest\\"					# Folder to store log files
+filename = "T_H_Log_"							# Log file name
+
+exit = False									# Flag to tell the program when to exit
+##############################################
+# Function declarations
+##############################################
+
+# Function to measure temp & humidity, and write to log file
+# Serial comms with Arduino (running TempSensor.ino). Sends instructions & receives data as a string.
+# Creates a new file for each day, and writes time,temp,RH to file
+# This is called by 'repeat' every time a measurement is required
+def measure():
+	global today, myFilename, lastMeasurement
+	
+	# Only allow a measurement every 0.5s to stop errors
+	if (time.time() - lastMeasurement) > 0.49:
+		s = time.strftime("%Y-%m-%d")
+		if today != s:							# If the day has changed
+			today = s							# Update the day
+			# Update filename
+			myFilename = folder + filename + today + ".txt"
+			# Write header row in new file
+			myFile = open(myFilename, 'w')
+			myFile.write("Time\tT\tRH\n")
+			myFile.close()
+		
+		mySerial.write("M")						# Send signal to take a measurement
+		temp = mySerial.readline()				# First line output from Arduino is the temperature
+		RH = mySerial.readline()				# Second line is the relative humidity
+		
+		temp = temp[4:9]						# Extract temp reading only
+		
+		if RH[3:7] == "Error":					# Make sure there was no error
+			RH = "Error" + RH[8:]				# If so, extract error code
+		else:
+			RH = RH[5:10]						# If not, extract RH reading only
+		
+		# Create string of tab separated timestamp, temp, RH
+		s = time.strftime("%H:%M:%S")
+		s += "\t" + temp + "\t" + RH + "\n"
+		
+#		print(s)								# Print for debugging
+		
+		# Write string to file
+		myFile = open(myFilename, 'a')
+		myFile.write(s)
+		myFile.close()
+		
+		lastMeasurement = time.time()			# Store time of measurement
+
+# Wrapper function to call myFunction every myInterval seconds, until exit flag is set	
+def repeat(myInterval, myFunction ):
+	global exit
+	if exit == False:							# Check that exit flag is not set
+		global myTimer
+		# Restart timer to call this same function again in myInterval seconds
+		myTimer = threading.Timer( myInterval, repeat, [myInterval, myFunction] )
+		myTimer.start()
+		myFunction()							# Call required function
+	else:
+		print "Stopping timer"
+		
+# Function to change the measurement interval based on user input
+# This is called by 'get_instruction' when the user chooses to change measurement interval
+def setInterval():
+	global myTimer
+	myTimer.cancel()							# Stop existing timer
+	
+	newInterval = raw_input('Choose new interval (s):')		# Get user input
+	interval = float(newInterval)				# Set new interval in global variable
+	
+	# Print confirmation of new interval for user
+	s = "Measurement interval " + repr(interval) + "s"
+	print s
+	
+	repeat(interval, measure )					# Start timer repeating
+
+# Function to prompt user for instructions, read user input and interpret
+def get_instruction():
+	# Get user input
+	instruction = raw_input('Type instructions (Q to quit, I to set new measurement interval):\n')
+	if instruction == "q" or instruction == "Q":
+		print("Quit command received")
+		global exit
+		exit = True								# Set exit flag - this will be detected next time 'repeat' is run
+	elif instruction == "i" or instruction == "I":
+		setInterval()							# Call function to set a new measurement interval
+		
+##############################################
+
+
+mySerial = serial.Serial('COM5')				# Open serial port
+# Print message for user
+s = "Serial port " + mySerial.name + " opened"
+print s
+
+# When serial port is opened, Arduino resets. Delay to ensure commands are received properly.
+time.sleep(2)	
+
+today = time.strftime("%Y-%m-%d")				# Store today's date - need to compare to see when we need to create a new file
+myFilename = folder + filename + today + ".txt" # Create string for filename
+lastMeasurement = time.time()					# Store timestamp of last measurement
+
+# Print measurement interval for user
+s = "Measurement interval " + repr(interval) + "s"
+print(s)
+
+# Write header row to file initially
+myFile = open(myFilename, 'w')
+myFile.write("Time\tT\tRH\n")
+myFile.close()
+
+repeat(interval, measure)						# Call 'repeat' to execute 'measure' every 'interval' seconds
+
+while exit == False:							# Repeat this until exit flag is set
+	# Create new thread for receiving input, so that measurements can still be made concurrently
+	inputThread = threading.Thread(target=get_instruction)
+	inputThread.start()
+	inputThread.join()							# Wait for thread to end (input to be received)
